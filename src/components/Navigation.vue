926ef559fa4856407ba9ca691e94bba30b8ec1e2
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-09-22 17:27:22 -0400

    [FEATURE] #1 - changing menu display for tablets

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,31 @@
 <template>
   <nav>
-    <div class="link">
-      <router-link to="/"><i class="fas fa-home fa-2x"></i>
-        <p>Home</p></router-link>
-    </div>
-    <div class="vertical-bar">
-      <span>|</span>
-    </div>
-    <div class="link">
-      <router-link to="/album"><i class="fas fa-compact-disc fa-2x"></i>
-        <p>Album</p></router-link>
-    </div>
-    <div class="vertical-bar">
-      <span>|</span>
-    </div>
-    <div class="link">
-      <router-link to="/artist"><i class="fas fa-user-astronaut fa-2x"></i>
-        <p>Artist</p></router-link>
-    </div>
-    <div class="vertical-bar">
-      <span>|</span>
-    </div>
-    <div class="link">
-      <router-link to="/playlist"><i class="fas fa-headphones fa-2x"></i>
-        <p>Playlist</p></router-link>
+    <div id="bar">
+      <div class="link">
+        <router-link to="/"><i class="fas fa-home fa-2x"></i>
+          <p>Home</p></router-link>
+      </div>
+      <div class="vertical-bar">
+        <span>|</span>
+      </div>
+      <div class="link">
+        <router-link to="/album"><i class="fas fa-compact-disc fa-2x"></i>
+          <p>Album</p></router-link>
+      </div>
+      <div class="vertical-bar">
+        <span>|</span>
+      </div>
+      <div class="link">
+        <router-link to="/artist"><i class="fas fa-user-astronaut fa-2x"></i>
+          <p>Artist</p></router-link>
+      </div>
+      <div class="vertical-bar">
+        <span>|</span>
+      </div>
+      <div class="link">
+        <router-link to="/playlist"><i class="fas fa-headphones fa-2x"></i>
+          <p>Playlist</p></router-link>
+      </div>
     </div>
   </nav>
 </template>
@@ -31,12 +33,16 @@
 <style>
   nav {
     height: 7em;
+    width: 100%;
     background: rgba(21, 54, 65, 1);
+    padding: 2.1em 0;
+  }
+
+  #bar {
+    width: 100%;
     display: flex;
     justify-content: flex-end;
     align-items: center;
-    width: 100%;
-    padding: 2.1em 0;
   }
 
   .vertical-bar {
@@ -49,7 +55,6 @@
   }
 
   .link {
-    /*white-space: nowrap;*/
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -79,13 +84,18 @@
     margin: 0.5em;
   }
 
-  @media only screen and (max-width: 1200px) {
+  @media only screen and (max-device-width: 1200px) {
+    nav {
+      height: 2em;
+    }
 
+    #bar {
+      justify-content: space-between;
+    }
+
+    .fas {
+      display: none;
+    }
   }
-
-  @media only screen and (max-width: 750px) {
-
-  }
-
 
 </style>
