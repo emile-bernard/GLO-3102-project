c74d792ad40246f1a6c5d781579f95051f44ad91
Emile05 <emile.bernard.2@ulaval.ca>
2018-09-22 16:08:30 -0400

    [FEATURE] #1 - Created desktop nav menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,18 +1,91 @@
 <template>
-    <nav>
-        <ul>
-            <li>
-                <router-link to="/">Home</router-link>
-            </li>
-            <li>
-                <router-link to="/album">Album</router-link>
-            </li>
-            <li>
-                <router-link to="/artist">Artist</router-link>
-            </li>
-        </ul>
-    </nav>
+  <nav>
+    <div class="link">
+      <router-link to="/"><i class="fas fa-home fa-2x"></i>
+        <p>Home</p></router-link>
+    </div>
+    <div class="vertical-bar">
+      <span>|</span>
+    </div>
+    <div class="link">
+      <router-link to="/album"><i class="fas fa-compact-disc fa-2x"></i>
+        <p>Album</p></router-link>
+    </div>
+    <div class="vertical-bar">
+      <span>|</span>
+    </div>
+    <div class="link">
+      <router-link to="/artist"><i class="fas fa-user-astronaut fa-2x"></i>
+        <p>Artist</p></router-link>
+    </div>
+    <div class="vertical-bar">
+      <span>|</span>
+    </div>
+    <div class="link">
+      <router-link to="/playlist"><i class="fas fa-headphones fa-2x"></i>
+        <p>Playlist</p></router-link>
+    </div>
+  </nav>
 </template>
 
 <style>
+  nav {
+    height: 7em;
+    background: rgba(21, 54, 65, 1);
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 100%;
+    padding: 2.1em 0;
+  }
+
+  .vertical-bar {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    color: rgba(148, 207, 201, 1);
+    font-weight: bold;
+    font-size: 1.1em;
+  }
+
+  .link {
+    /*white-space: nowrap;*/
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    height: 100%;
+    font-family: monospace;
+    font-size: 1.5em;
+    padding: 0 2.1em;
+  }
+
+  .link:hover {
+    background: rgba(34, 85, 110, 1);
+    font-weight: bold;
+  }
+
+  .link:hover > a {
+    color: white;
+  }
+
+  a {
+    color: rgba(148, 207, 201, 1);
+    text-decoration: none;
+  }
+
+  p {
+    margin: 0.5em;
+  }
+
+  @media only screen and (max-width: 1200px) {
+
+  }
+
+  @media only screen and (max-width: 750px) {
+
+  }
+
+
 </style>
