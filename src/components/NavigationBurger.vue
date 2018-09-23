d27021e73198bb10ead4649ad8014f0746cd8af4
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-09-23 14:50:29 -0400

    [FEATURE] #1 - creating component for navigation burger

---
+++ src/components/NavigationBurger.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="navigation-burger">
+    <navigation-burger-menu></navigation-burger-menu>
+    <navigation-burger-icon></navigation-burger-icon>
+  </div>
+</template>
+
+<style>
+  #navigation-burger {
+    display: none;
+    overflow: auto;
+  }
+
+  @media only screen and (max-device-width: 750px) {
+    #navigation-burger {
+      display: block;
+    }
+  }
+</style>
+
+<script>
+  import NavigationBurgerIcon from '@/components/NavigationBurgerIcon';
+  import NavigationBurgerMenu from '@/components/NavigationBurgerMenu';
+
+  export default {
+    components: {
+      'navigation-burger-icon': NavigationBurgerIcon,
+      'navigation-burger-menu': NavigationBurgerMenu
+    },
+  };
+</script>
