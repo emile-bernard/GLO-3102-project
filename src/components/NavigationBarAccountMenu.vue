6e2ace4491f85520c10b131e1dd75fa1d234dd82
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-09-27 22:18:02 -0400

    [FEATURE] #1 - reordering the items into the accountMenu

--- src/components/NavigationBarAccountMenu.vue
+++ src/components/NavigationBarAccountMenu.vue
@@ -9,13 +9,13 @@
       </a>
       <div class="navbar-dropdown is-right">
         <a class="navbar-item">
-          <router-link to="/settings">
-            <a href="#"><i class="fas fa-cog"></i>&nbsp;Settings</a>
+          <router-link to="/account">
+            <a href="#" id="user-name">Uncle Bob</a>
           </router-link>
         </a>
         <a class="navbar-item">
-          <router-link to="/account">
-            <a href="#" id="user-name">Uncle Bob</a>
+          <router-link to="/settings">
+            <a href="#"><i class="fas fa-cog"></i>&nbsp;Settings</a>
           </router-link>
         </a>
         <hr class="navbar-divider">
