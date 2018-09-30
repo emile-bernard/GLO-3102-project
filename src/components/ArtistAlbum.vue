037889172716632ffa88fbec2ca63038dfd3c59a
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-09-30 02:57:35 -0400

    [FEATURE] #3 - Creating a specific component for Artist albums + parsing src file + entry data + output data + staging api request comment

---
+++ src/components/ArtistAlbum.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="artist-album">
+    <div><h3><b>{{title}}</b></h3>
+      <p>{{genre}}</p><a :href=refLink><img
+        :src=imgSrc></a>
+      <p>{{copyright}}</p></div>
+  </div>
+</template>
+
+<style lang="scss">
+  @import "~bulma/bulma.sass";
+  @import "~bulmaswatch/superhero/bulmaswatch.scss";
+
+  .artist-album {
+    background-color: rgba(0, 0, 0, 0.1);
+    width: 200px;
+    margin: 10px;
+    padding: 10px;
+    color: $grey-lighter;
+  }
+
+  .artist-album div img {
+    width: 100%;
+  }
+</style>
+
+<script>
+  export default {
+    props: ['title', 'genre', 'refLink', 'imgSrc', 'copyright'],
+  };
+</script>
