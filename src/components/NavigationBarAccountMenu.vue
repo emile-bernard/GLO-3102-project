76337e6f2dbf9636a28e8296bbe9e7b5d119ef92
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-09-30 22:44:43 -0400

    [FEATURE] - unclickable username in dropdown

--- src/components/NavigationBarAccountMenu.vue
+++ src/components/NavigationBarAccountMenu.vue
@@ -1,28 +1,24 @@
 <template>
-  <router-link to="/account">
-    <div class="navbar-item has-dropdown is-hoverable">
-      <i class="fas fa-user fa-2x"></i>
-      <p>Account</p>
-      <div class="navbar-dropdown">
-        <router-link to="/account">
-          <div class="navbar-item">
-            <a href="#" id="user-name">Uncle Bob</a>
-          </div>
-        </router-link>
-        <router-link to="/settings">
-          <div class="navbar-item">
-            <a href="#"><i class="fas fa-cog"></i>&nbsp;Settings</a>
-          </div>
-        </router-link>
-        <hr class="navbar-divider">
-        <router-link to="/logout">
-          <div class="navbar-item">
-            <a href="#"><i class="fas fa-sign-out-alt"></i>&nbsp;Log out</a>
-          </div>
-        </router-link>
+  <div class="navbar-item has-dropdown is-hoverable">
+    <i class="fas fa-user fa-2x"></i>
+    <p>Account</p>
+    <div class="navbar-dropdown">
+      <div id="user-name">
+        <a href="#">Uncle Bob</a>
       </div>
+      <router-link to="/settings">
+        <div class="navbar-item">
+          <a href="#"><i class="fas fa-cog"></i>&nbsp;Settings</a>
+        </div>
+      </router-link>
+      <hr class="navbar-divider">
+      <router-link to="/logout">
+        <div class="navbar-item">
+          <a href="#"><i class="fas fa-sign-out-alt"></i>&nbsp;Log out</a>
+        </div>
+      </router-link>
     </div>
-  </router-link>
+  </div>
 </template>
 
 <style>
@@ -40,6 +36,11 @@
 
   .navbar-dropdown .navbar-item:hover {
     background: rgba(34, 85, 110, 1);
+  }
+
+  #user-name a {
+    color: white;
+    font-weight: bold;
   }
 </style>
 
