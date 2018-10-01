366dec5dd2dbbee8a836381c8fd260d155a583b8
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-10-01 00:29:27 -0400

    [DOC] - staging final version delivery 1

--- admin/livrable1/delivery/ubeatEquipe9/src/components/Logout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Logout</h1>
-    <div>GLO-3102 Logout page</div>
-  </div>
-</template>
-
-<style>
-</style>
--- admin/livrable1/delivery/ubeatEquipe9/src/components/Playlist.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <h1>Playlist</h1>
-    <div>GLO-3102 Playlist page</div>
-  </div>
-</template>
-
-<style>
-</style>
