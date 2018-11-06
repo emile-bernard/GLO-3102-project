ffe4af0d57ef493d4a3f39e109a44f5f9028eb51
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-11-06 15:31:41 -0500

    [feature] #21 - first draft for playlist main page

--- src/components/Playlist/PlaylistList.vue
+++ src/components/Playlist/PlaylistList.vue
@@ -1,6 +1,10 @@
 <template>
-
-
+  <section class="section">
+    <playlist-overview v-for="(playlist, index) in playlists"
+                       v-bind:key=index
+                       v-bind:name="playlist.name">
+    </playlist-overview>
+  </section>
 </template>
 
 <style>
@@ -8,5 +12,35 @@
 </style>
 
 <script>
+  import PlaylistOverview from './PlaylistOverview';
 
+  export default {
+    data() {
+      return {
+        playlists: [],
+      };
+    },
+    components: {
+      'playlist-overview': PlaylistOverview,
+    },
+    methods: {
+      filterPlaylists(allPlaylists) {
+        for (let i = 0; i < 10; i += 1) {
+          this.populatePlaylists(allPlaylists[i]);
+        }
+      },
+      populatePlaylists(playlist) {
+        fetch(`https://ubeat.herokuapp.com/unsecure/playlists/${playlist.id}`, { method: 'get' })
+            .then(response => response.json())
+            .then(response => this.playlists.push({ name: response.id, tracks: playlist.tracks }));
+      },
+    },
+    created() {
+      fetch('https://ubeat.herokuapp.com/unsecure/playlists', { method: 'get' })
+        .then(response => response.json())
+        .then((response) => {
+          this.filterPlaylists(response);
+        });
+    }
+  };
 </script>
---
+++ src/components/Playlist/PlaylistOverview.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent is-vertical is-4">
+      <div class="tile is-horizontal">
+        <div class="tile">
+          <div class="tile is-parent is-vertical">
+            <article class="tile is-child notification is-primary">
+              <p class="title" contenteditable="true">{{name}}</p>
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/640x480.png">
+              </figure>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  export default {
+    props: {
+      name: undefined,
+      tracks: [],
+    }
+  };
+</script>
