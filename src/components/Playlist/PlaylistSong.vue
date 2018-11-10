dfc4ee75789a72acf5c97452fc276f63290b3fed
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2018-11-10 14:23:58 -0500

    [feature] #21 - embedding audio controls into webpage

--- src/components/Playlist/PlaylistSong.vue
+++ src/components/Playlist/PlaylistSong.vue
@@ -1,28 +1,32 @@
 <template>
-  <a id="playlist-song-link" class="panel-block">
+  <div id="playlist-song-link" class="panel-block">
     <span class="panel-icon">
       <i class="fas fa-music" aria-hidden="true"></i>
     </span>
     {{trackNumber}}. {{trackName}} ({{Math.floor(trackTimeMillis/60000)}}:{{((trackTimeMillis%60000)/1000).toFixed(0)}})
     &nbsp;
-    <a class="button is-rounded is-primary" @click="playSong" :href=previewUrl rel="nofollow">
-      <i class="fas fa-play-circle action" aria-hidden="true"></i>
-    </a>
+    <audio controls>
+      <source :src="previewUrl">
+    </audio>
     <a class="button is-rounded is-danger" @click="removeSong">
       <i class="fas fa-trash action" aria-hidden="true"></i>
     </a>
-  </a>
+  </div>
 </template>
 
 <style>
-  .panel-icon {
-    color: white;
-  }
-
   #playlist-song-link:hover {
     cursor: pointer;
     color: white;
     background-color: rgba(160, 160, 160, 0.8);
+  }
+
+  .panel-icon {
+    color: white;
+  }
+
+  audio {
+    min-height: 60px;
   }
 
   .fas.action {
@@ -58,9 +62,6 @@
       trackTimeMillis: undefined,
     },
     methods: {
-      playSong() {
-        // Todo
-      },
       removeSong() {
         fetch(`https://ubeat.herokuapp.com/unsecure/playlists/${this.playlistId}/tracks/${this.trackId}`,
           {
