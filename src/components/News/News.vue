da0cc973bfa9c66f68f0e55ebb318f302ad58993
Emile05 <emile.bernard.2@ulaval.ca>
2018-11-30 12:53:43 -0500

    [feature] #29 - Fixed news page articles display

--- src/components/News/News.vue
+++ src/components/News/News.vue
@@ -16,127 +16,20 @@
     </section>
 
     <div class="section">
-      <div class="box">
-        <div class="field has-addons">
-          <div class="control is-expanded">
-            <input class="input has-text-centered" type="search" placeholder="» » » » » » find me « « « « « «">
-          </div>
-          <div class="control">
-            <a class="button is-info">Search</a>
-          </div>
-        </div>
-      </div>
-
-      <!-- Developers -->
-      <div class="row columns">
-        <!--<div class="column is-one-third">-->
-        <!--<div class="card large">-->
-        <!--<div class="card-image">-->
-        <!--<figure class="image">-->
-        <!--<img-->
-        <!--src="https://images.unsplash.com/photo-1475778057357-d35f37fa89dd?dpr=1&auto=compress,format&fit=crop&w=1920&h=&q=80&cs=tinysrgb&crop="-->
-        <!--alt="Image">-->
-        <!--</figure>-->
-        <!--</div>-->
-        <!--<div class="card-content">-->
-        <!--<div class="media">-->
-        <!--<div class="media-left">-->
-        <!--<figure class="image is-96x96">-->
-        <!--<img src="https://i.imgsafe.org/a4/a4bb9acc5e.jpeg" alt="Image">-->
-        <!--</figure>-->
-        <!--</div>-->
-        <!--<div class="media-content">-->
-        <!--<p class="title is-4 no-padding">Okinami</p>-->
-        <!--<p><span class="title is-6"><a href="http://twitter.com/#">@twitterid</a></span></p>-->
-        <!--<p class="subtitle is-6">Lead Developer</p>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="content">-->
-        <!--The Beast stumbled in the dark for it could no longer see the path. It started to fracture and weaken,-->
-        <!--trying to reshape itself into the form of metal. Even the witches would no longer lay eyes upon it, for-->
-        <!--it had become hideous and twisted.-->
-        <!--<div class="background-icon"><span class="icon-twitter"></span></div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="column is-one-third">-->
-        <!--<div class="card large">-->
-        <!--<div class="card-image">-->
-        <!--<figure class="image">-->
-        <!--<img src="https://source.unsplash.com/uzDLtlPY8kQ" alt="Image">-->
-        <!--</figure>-->
-        <!--</div>-->
-        <!--<div class="card-content">-->
-        <!--<div class="media">-->
-        <!--<div class="media-left">-->
-        <!--<figure class="image is-96x96">-->
-        <!--<img-->
-        <!--src="https://cdn.discordapp.com/avatars/244932903530659841/0c924a19fcf1b5c59bc9dc1b58b61bb0.jpg?size=1024"-->
-        <!--alt="Image">-->
-        <!--</figure>-->
-        <!--</div>-->
-        <!--<div class="media-content">-->
-        <!--<p class="title is-4 no-padding">McSocks</p>-->
-        <!--<p><span class="title is-6"><a href="http://twitter.com/#">@twitterid</a></span></p>-->
-        <!--<p class="subtitle is-6">Developer</p>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="content">-->
-        <!--The soul of the Beast seemed lost forever. Then, by the full moon's light, a child was born; child with-->
-        <!--the unbridled soul of the Beast that would make all others pale in comparison.-->
-        <!--<div class="background-icon"><span class="icon-facebook"></span></div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="column is-one-third">-->
-        <!--<div class="card large">-->
-        <!--<div class="card-image">-->
-        <!--<figure class="image">-->
-        <!--<img src="https://source.unsplash.com/pe_R74hldW4" alt="Image">-->
-        <!--</figure>-->
-        <!--</div>-->
-        <!--<div class="card-content">-->
-        <!--<div class="media">-->
-        <!--<div class="media-left">-->
-        <!--<figure class="image is-96x96">-->
-        <!--<img src="https://i.imgsafe.org/a5/a5e978ce20.jpeg" alt="Image">-->
-        <!--</figure>-->
-        <!--</div>-->
-        <!--<div class="media-content">-->
-        <!--<p class="title is-4 no-padding">The Conceptionist</p>-->
-        <!--<p><span class="title is-6"><a href="http://twitter.com/#">@twitterid</a></span></p>-->
-        <!--<p class="subtitle is-6">Developer</p>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--<div class="content">-->
-        <!--Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad-->
-        <!--amet inventore hic beatae, quas accusantium perferendis sapiente explicabo, corporis totam! Labore-->
-        <!--reprehenderit beatae magnam animi!-->
-        <!--<div class="background-icon"><span class="icon-barcode"></span></div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-        <!--</div>-->
-
-
-        <news-article v-for="(item, index) in newsArticles"
-                      v-bind:key=index
-                      v-bind:articleAuthor="item.articleAuthor"
-                      v-bind:articleContent="item.articleContent"
-                      v-bind:articleDescription="item.articleDescription"
-                      v-bind:articlePublishedAt="item.articlePublishedAt"
-                      v-bind:articleSource="item.articleSource"
-                      v-bind:articleTitle="item.articleTitle"
-                      v-bind:articleUrl="item.articleUrl"
-                      v-bind:articleUrlToImage="item.articleUrlToImage">
-        </news-article>
-
-      </div>
-      <!-- End Developers -->
+      <news-article v-for="(item, index) in newsArticles"
+                    v-bind:key=index
+                    v-bind:articleAuthor="item.articleAuthor"
+                    v-bind:articleContent="item.articleContent"
+                    v-bind:articleDescription="item.articleDescription"
+                    v-bind:articlePublishedAt="item.articlePublishedAt"
+                    v-bind:articleSource="item.articleSource"
+                    v-bind:articleTitle="item.articleTitle"
+                    v-bind:articleUrl="item.articleUrl"
+                    v-bind:articleUrlToImage="item.articleUrlToImage">
+      </news-article>
     </div>
   </div>
+
 </template>
 
 <style>
--- src/components/News/NewsArticle.vue
+++ src/components/News/NewsArticle.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="column is-one-third">
-    <div class="card large">
-      <div class="card-image">
-        <figure class="image">
-          <img v-bind:src=articleUrlToImage alt="Article Image">
-        </figure>
+  <div id="news-article-card" class="card large">
+    <div class="card-image">
+      <figure class="image">
+        <img v-bind:src=articleUrlToImage alt="Article Image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <i class="fas fa-at fa-6x"></i>
+        </div>
+        <div class="media-content">
+          <p class="title is-4 no-padding">{{ articleAuthor }}</p>
+          <p><span class="title is-6">{{ articleSource.name }}</span></p>
+          <p class="subtitle is-6">{{ articlePublishedAt }}</p>
+        </div>
       </div>
-      <div class="card-content">
-        <div class="media">
-          <div class="media-left">
-            <figure class="image is-96x96">
-              <img
-                src="https://cdn.discordapp.com/avatars/244932903530659841/0c924a19fcf1b5c59bc9dc1b58b61bb0.jpg?size=1024"
-                alt="Image">
-            </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-4 no-padding">{{ articleAuthor }}</p>
-            <p><span class="title is-6"><a href="http://twitter.com/#">{{ articleSource.name }}</a></span></p>
-            <p class="subtitle is-6">{{ articlePublishedAt }}</p>
-          </div>
-        </div>
-        <div class="content">
-          <p class="tile">{{ articleTitle }}</p>
-          <p class="subtitle">{{ articleDescription }}</p>
-          <p>{{ articleContent }}</p>
-          <p><span class="title is-6"><a v-bind:href=articleUrl>Read More</a></span></p>
-        </div>
+      <div class="content">
+        <p class="tile">{{ articleTitle }}</p>
+        <p class="subtitle">{{ articleDescription }}</p>
+        <p>{{ articleContent }}</p>
+        <p>
+          <span class="title is-6">
+            <a v-bind:href=articleUrl target="_blank">Read More&nbsp;<i class="fas fa-glasses"></i></a>
+
+          </span>
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <style>
-
+  #news-article-card {
+    margin-bottom: 2em;
+  }
 </style>
 
 <script>
