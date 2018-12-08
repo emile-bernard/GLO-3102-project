0d28ae08703149c0bab6091a607de0154dd6b992
Pamella <pamellalawless@gmail.com>
2018-12-08 17:05:56 -0500

    Clean user page

--- src/components/User/UserInformations.vue
+++ src/components/User/UserInformations.vue
@@ -6,30 +6,18 @@
       We might want to set the username and email in a new cookie too...-->
 
       <figure class="card-image is-centered card-header-icon ">
+        <h2 class="title"><b>Gravatar</b></h2>
         <v-gravatar
-          :email="email"></v-gravatar>
+          :email="email"
+         ></v-gravatar>
       </figure>
+      <a  href="https://fr.gravatar.com/">Not register to Gravatar.com? Create yout Gravatar account!</a>
     </div>
     <br>
     <div class="account-info-element">
       <h2 class="title"><b>Email</b> <p>{{email}}</p></h2>
-      <p>
-        <button class="button is-primary">Change Your Email</button>
-      </p>
     </div>
     <br>
-    <div class="account-info-element">
-      <h2 class="title"><b>Password</b></h2>
-      <button class="button is-primary">Change Your Password</button>
-    </div>
-    <br>
-    <div class="account-info-element">
-      <h2 class="title"><b>Volume Settings</b></h2>
-      <input id="sliderWithValue" class="slider has-output is-fullwidth"
-             min="0" max="100" value="50" step="1" type="range">
-      <output for="sliderWithValue"></output>
-    </div>
-  </div>
   </div>
 </template>
 
